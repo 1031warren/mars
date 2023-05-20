4e7824ed5b39018aed63fb5e44d5b32cc8b9a47a
Adam Warren <113055287+1031warren@users.noreply.github.com>
2023-05-20 13:23:02 -0700

    Update mars.php

---
+++ mars.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mars Tourism</title>
+    <script src="mars.js"></script>
+    <link rel="stylesheet" href="mars.css">
+</head>
+<body>
+    <h1>Welcome to the website.</h1> 
+
+    <p> 
+        Perpare yourself for the journey of a lifetime to the planet mars.
+        But do not forget once we depart there is no going back.  
+     <p>
+
+     <?php
+        echo "<p>This is Mars tourism website.</p>";
+     ?>
+</body>
+</html>
